8afa20b01cb580544acd340671c5224a634d0087
Antony Samarawickrema <antonysama@gmail.com>
2019-09-05 22:02:29 -0600

    Update sum.py

--- sum.py
+++ sum.py
@@ -3,9 +3,25 @@
 # For expediency I reduced these down by 1/50--max_enc_steps=400 --max_dec_steps=120...NEXT TIME reduce epoch size , too.
 # Run the following
 
-python run_summarization.py --mode=train --data_path=/home/antony/environments/n/cnn-dailymail/finished_files/chunked/val_* --vocab_path=/home/antony/environments/n/cnn-dailymail/finished_files/vocab --log_root=/home/antony/environments/n/cnn-dailymail/abstracter_model/ --exp_name=abstracter_model --max_enc_steps=8 --max_dec_steps=4 --coverage=1
+#python pointer-generator-master/run_summarization.py\
+  --mode=decode --data_path=/path/to/your/finished_test_files/chunked/test_* 
+  --vocab_path=vocab 
+  --log_root=/path/to/pretrained_model 
+  --exp_name=pretrained_model_ 
+  --coverage=1 
+  --single_pass=1 
+  --max_enc_steps=500 
+  --max_dec_steps=200 
+  --min_dec_steps=100
+  
+  python run_summarization.py\
+  --mode=train 
+  --data_path=/home/antony/environments/n/cnn-dailymail/finished_files/chunked/val_* 
+  --vocab_path=/home/antony/environments/n/cnn-dailymail/finished_files/vocab 
+  --log_root=/home/antony/environments/n/cnn-dailymail/abstracter_model/ 
+  --exp_name=abstracter_model --max_enc_steps=8 --max_dec_steps=4 --coverage=1
 
-#python pointer-generator-master/run_summarization.py --mode=decode --data_path=/path/to/your/finished_test_files/chunked/test_* --vocab_path=vocab --log_root=/path/to/pretrained_model --exp_name=pretrained_model_ --coverage=1 --single_pass=1 --max_enc_steps=500 --max_dec_steps=200 --min_dec_steps=100
-
-#If you want to get the results of the pretrained models, set two arguments in the scripts:  set the MODE to evalall (i.e., MODE='evalall'). Set the CKPT_PATH to our pretrained model (e.g., CKPT_PATH="pretrained/bestmodel-xxxx").
+# If you want to get the results of the pretrained models, set two arguments in the scripts:  
+# set the MODE to evalall (i.e., MODE='evalall'). 
+# Set the CKPT_PATH to our pretrained model (e.g., CKPT_PATH="pretrained/bestmodel-xxxx").
 
