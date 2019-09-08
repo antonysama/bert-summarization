02b32b159d868ef4cb1a59bc9f1ed0e8a70c9f10
Antony Samarawickrema <antonysama@gmail.com>
2019-09-07 23:51:54 -0600

    Update notes for data.py

--- utils/notes for data.py
+++ utils/notes for data.py
@@ -4,3 +4,20 @@ pip2 install pytorch-pretrained-bert --user
 pip2 install python-utils
 
 *
+pip2 install\
+    numpy \
+    scipy \
+    pandas \
+pvlib==0.6.0 \
+    cloudpickle \
+pickleshare \
+    scikit-learn==0.20.0 \
+python-dateutil==2.7.5 \
+    matplotlib \
+PyMySQL==0.9.2 \
+mkl-fft \
+mkl-random \
+windrose==1.6.5 \
+    Cython \
+tables==3.4.4 \
+pyrouge==0.1.3 \
