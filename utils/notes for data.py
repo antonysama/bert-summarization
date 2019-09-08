021db8f9ae13b32fe56d5356add5d308a97d58a8
Antony Samarawickrema <antonysama@gmail.com>
2019-09-07 23:52:24 -0600

    Update notes for data.py

--- utils/notes for data.py
+++ utils/notes for data.py
@@ -1,7 +1,7 @@
 pip2 install -r requirements.txt  # plus a few others *
 pip2 install torch torchvision --user
 pip2 install pytorch-pretrained-bert --user
-pip2 install python-utils
+pip2 install python-utils --user
 
 *
 pip2 install\
